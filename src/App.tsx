24951c8f8226f5cdc85aa75eab0bac820e23c40e
Ikubanni Paul <ipom4eva@gmail.com>
2022-11-10 07:12:04 +0100

    Refactoring the Routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
 
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import StatusComponent from './components/Dashboard/elements/StatusComponent';
 import { Dashboard, Login, UserDetail } from './pages/';
 function App()
@@ -8,18 +8,17 @@ function App()
 
   return (
     <div className="app">
-      <Routes>
+
+
+      <div><Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/users" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path='/dashboard/users/:id' element={<UserDetail />} />
         <Route path='/blacklist/:id' element={<StatusComponent status="blacklisted" message="Blacklisted successfully" />} />
-      </Routes>
-      <div>
-        Home Page
-        < Link to="/login" > Login Here</Link >
-        <Link to="/dashboard/users">Visit our Users Dashboard</Link>
-      </div >
+      </Routes></div>
+
     </div>
   )
 }
